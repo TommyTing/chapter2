8f612a7f66ffa2b834ce67d4b65313397e83b05a
TommyTing <1750132354@qq.com>
2017-12-15 12:44:58 +0800

    qqq

--- src/main/java/org/smart4j/service/CustomerService.java
+++ src/main/java/org/smart4j/service/CustomerService.java
@@ -21,46 +21,36 @@ public class CustomerService {
      * 获取客户列表
      */
     public List<Customer> getCustomerList(){
-        Connection conn=DatabaseHelper.getConnection();
-        try {
-            String sql="SELECT * FROM customer";
-            return DatabaseHelper.queryEntityList(Customer.class,conn,sql);
-        }catch (SQLException e){
-            PropsUtil.LOGGER.error("excute sql failure",e);
-        }finally {
-           DatabaseHelper.closeConnection(conn);
-        }
+        String sql="SELECT * FROM customer";
+        return DatabaseHelper.queryEntityList(Customer.class,sql);
     }
 
     /**
      * 获取客户
      */
     public Customer getCustomer(long id){
-        //TODO
-        return  null;
+        String sql="SELECT * FROM customer WHERE id=";
+        return  DatabaseHelper.queryEntity(Customer.class,sql,id);
     }
 
     /**
      * 创建客户
      */
     public boolean createCustomer(Map<String,Object> fieldMap){
-        //TODO
-        return  false;
+        return  DatabaseHelper.insertEntity(Customer.class,fieldMap);
     }
 
     /**
      * 更新客户
      */
     public boolean updateCustomer(long id,Map<String,Object> fieldMap){
-        //TODO
-        return  false;
+        return DatabaseHelper.updateEntity(Customer.class,id,fieldMap);
     }
 
     /**
      * 删除客户
      */
     public boolean deleteCustomer(long id){
-        //TODO
-        return  false;
+        return DatabaseHelper.deleteEntity(Customer.class,id);
     }
 }
